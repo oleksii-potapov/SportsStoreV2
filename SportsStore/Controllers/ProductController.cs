62445f20d22a989efb6d97056f48b39eb3379c03
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-23 11:13:19 +0300

    Add ProductController

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
--- SportsStore/Models/IProductRepository.cs
+++ SportsStore/Models/IProductRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace SportsStore.Models
 {
-    internal interface IProductRepository
+    public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
     }
