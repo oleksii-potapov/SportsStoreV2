2d32cc52ec094257b2b1f7e2c44293b5dc58f77b
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-23 13:52:19 +0300

    add pages into the product controller

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -10,15 +10,19 @@ namespace SportsStore.Controllers
     public class ProductController : Controller
     {
         private IProductRepository _repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repository)
         {
             _repository = repository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int productPage = 1)
         {
-            return View(_repository.Products);
+            return View(_repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
