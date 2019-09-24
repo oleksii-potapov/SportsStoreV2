2df260ec79424489730c260652e8a8b6cd29746b
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-24 11:16:45 +0300

    add route for pages

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -40,6 +40,11 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "pagination",
+                    template: "Products/Page{productPage}",
+                    defaults: new { controller = "Product", action = "List" });
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
             });
