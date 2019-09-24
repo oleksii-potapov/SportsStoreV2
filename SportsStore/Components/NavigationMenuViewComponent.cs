e8d293200947de57be82db22ef3980812c0dcce2
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-24 15:54:43 +0300

    add skeleton of view component for catgories

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
