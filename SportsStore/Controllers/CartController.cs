12d589d8ff785df05bba19fc1857497b3c4476b5
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-25 11:26:36 +0300

    Add Cart controller; add extension method for json cart

---
+++ SportsStore/Controllers/CartController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using SportsStore.Models;
+using SportsStore.Infrastructure;
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository _repository;
+
+        public CartController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = _repository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                CurrentCart.AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = _repository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (product != null)
+            {
+                CurrentCart.RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart CurrentCart
+        {
+            get => HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            set => HttpContext.Session.SetJson("Cart", value);
+        }
+    }
+}
