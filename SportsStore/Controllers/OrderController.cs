ec2aa7f8aeebac5991ab0efded50044ad6534f97
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-26 11:11:32 +0300

    edit methods of the Order controller

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -9,9 +9,44 @@ namespace SportsStore.Controllers
 {
     public class OrderController : Controller
     {
+        private IOrderRepository _repository;
+        private Cart _cart;
+
+        public OrderController(IOrderRepository repository, Cart cart)
+        {
+            _repository = repository;
+            _cart = cart;
+        }
+
         public ViewResult Checkout()
         {
             return View(new Order());
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (_cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = _cart.Lines.ToArray();
+                _repository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        public ViewResult Completed()
+        {
+            _cart.Clear();
+            return View();
         }
     }
 }
