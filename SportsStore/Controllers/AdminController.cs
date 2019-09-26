91daf414cd4fb7651b6f5371358c34fac9f93cba
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-26 13:43:15 +0300

    add Admin controller

---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository _repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
