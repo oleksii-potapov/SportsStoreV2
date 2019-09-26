8af248e22785c16d43ca8fd7fb28e26f4b95594b
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-26 15:41:53 +0300

    add Edit method (post) for saving data for products

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -26,5 +26,20 @@ namespace SportsStore.Controllers
             return View(_repository.Products
                 .FirstOrDefault(p => p.ProductId == productId));
         }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.SaveProduct(product);
+                TempData["message"] = $"{product.Name} has been saved.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
     }
 }
