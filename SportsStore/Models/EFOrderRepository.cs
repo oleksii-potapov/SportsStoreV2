b5faee65ca3a631dfe6de8d97bc09120a5534c25
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-26 10:22:50 +0300

    add implementation class of IOrderRepository

---
+++ SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext _context;
+
+        public EFOrderRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Order> Orders =>
+            _context.Orders
+            .Include(o => o.Lines)
+            .ThenInclude(l => l.Product);
+
+        public void SaveOrder(Order order)
+        {
+            _context.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.OrderId == 0)
+            {
+                _context.Orders.Add(order);
+            }
+            _context.SaveChanges();
+        }
+    }
+}
