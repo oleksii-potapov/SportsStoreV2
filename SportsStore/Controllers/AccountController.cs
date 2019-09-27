b56ea383c66eeba9511e997361832eef6c635bf2
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-27 15:00:28 +0300

    edit IdentitySeedData for better way of seeding

--- SportsStore/Controllers/AccountController.cs
+++ SportsStore/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
@@ -18,6 +19,7 @@ namespace SportsStore.Controllers
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            IdentitySeedData.EnsurePopulated(userManager).Wait();
         }
 
         [AllowAnonymous]
