203ea2740302ff2567f401631d9458ea62ba58bb
Oleksii Potapov <olexii.potapov@gmail.com>
2019-09-27 09:43:27 +0300

    add Delete method into the Admin controller

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -46,5 +46,16 @@ namespace SportsStore.Controllers
         {
             return View("Edit", new Product());
         }
+
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            Product deletedProduct = _repository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = $"{deletedProduct.Name} was deleted.";
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
